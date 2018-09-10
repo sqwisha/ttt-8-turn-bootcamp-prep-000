751b31e1be46c10fd2861fa55b806dce81882d26
S H <sashanicolehart@gmail.com>
2018-09-10 23:36:49 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, players_character="X")
+  board[index] = "#{players_character}"
+  return board
+end  
