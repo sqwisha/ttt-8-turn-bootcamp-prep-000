7b3160dcaa6cd4e10d5953d5052bbc821ecef9d2
S H <sashanicolehart@gmail.com>
2018-09-10 23:37:27 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,11 +6,23 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(input)
   input.to_i - 1
 end
 
+
 def move(board, index, players_character="X")
   board[index] = "#{players_character}"
   return board
+end  
+
+
+def valid_move?(board, index)
+  (index.between?(0,8) && !(position_taken?(board, index))) ? true : false
+end
+
+
+def position_taken?(board, index)
+  ! (board[index] == "" || board[index] == " " || board[index] == nil)
 end  
